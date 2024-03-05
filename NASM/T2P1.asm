1a1a4e2ffd2fe545fdb6a1a1b530ca8cf429435d
Mariana <mariananavarro3022@gmail.com>
2024-03-04 20:19:12 -0500

    comentarios

--- NASM/T2P1.asm
+++ NASM/T2P1.asm
@@ -9,12 +9,12 @@ section .text
 	global _main
 
 _main:
-	call _printText1
-	call _getText
-	call _compareTexts
+	call _printText1	;Imprime el texto inicial
+	call _getText		;Obtiene el texto del teclado
+	call _compareTexts	;compara las letras a ver si son mayusculas o minusculas
 
 
-_printText1:
+_printText1:			;texto inicial
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, text1
@@ -22,7 +22,7 @@ _printText1:
 	syscall 
 	ret
 
-_getText:
+_getText:			;obtiene el texto
 	mov rax, 0
 	mov rdi, 0
 	mov rsi, textoC
@@ -31,43 +31,42 @@ _getText:
 	ret
 
 _compareTexts:
-	mov al, byte[esi]
-	cmp al, 0xA
-	je _printFinalText
-	;je _finishCode
-	cmp al,'A'
-	jb _finishCodeError
-	cmp al,'Z'
-	ja _compareTextsLower
-	jmp _toLowerCase
+	mov al, byte[esi]	;obtiene el caracter menos significativo
+	cmp al, 0xA		;se asegura de que sea el final
+	je _printFinalText	;fin
+	cmp al,'A'		;le resta al caracter 41 si es menos de 41 entonces no es valido
+	jb _finishCodeError	;da error
+	cmp al,'Z'		;le resta al caracter 5A si es superior entonces puede ser que sea una letra minuscula
+	ja _compareTextsLower	;envia a check de minuscula
+	jmp _toLowerCase 	;cambia a minuscula el caracter
 
 _compareTextsLower:
-	cmp al,'a'
-	jb _finishCodeError
-	cmp al,'z'
-	ja _compareTextsLower
-	jmp _toUpperCase
+	cmp al,'a'		;le resta al caracter 61 si es menos de 61 entonces no es valido
+	jb _finishCodeError	;da error
+	cmp al,'z'		;le resta al caracter 7A si es superior entonces da error
+	ja _finishCodeError	;da error
+	jmp _toUpperCase	;cambia a mayuscula el caracter
 	
 	
-_toUpperCase:
+_toUpperCase:			;cambia a mayuscula
 	mov al, byte[esi]
 	cmp al, 0xA
 	je _printFinalText
-	sub al, 32
+	sub al, 32		;resta 32 bits al caracter volviendolo mayuscula
+	mov byte[esi],al
+	inc esi			;se obtiene el siguiente caracter
+	jmp _compareTexts
+
+_toLowerCase:			;cambia a minuscula
+	mov al, byte[esi]
+	cmp al, 0xA
+	je _printFinalText
+	add al, 32		;agrega 32 bits al caracter volviendolo minuscula
 	mov byte[esi],al
 	inc esi
 	jmp _compareTexts
 
-_toLowerCase:
-	mov al, byte[esi]
-	cmp al, 0xA
-	je _printFinalText
-	add al, 32
-	mov byte[esi],al
-	inc esi
-	jmp _compareTexts
-
-_printFinalText:
+_printFinalText:		;print final
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, textoC
@@ -75,18 +74,15 @@ _printFinalText:
 	syscall 
 	call _finishCode
 
-_finishCodeError:
+_finishCodeError:		;print de error
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, text2
 	mov rdx, 33
 	syscall 
+	call _finishCode
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
-
-_finishCode:
+_finishCode:			;finaliza codigo
 	mov rax, 60
 	mov rdi, 0
 	syscall
