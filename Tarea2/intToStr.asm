4245a09a534207cb9cec7958c40f4bb0b043e174
Mariana <mariananavarro3022@gmail.com>
2024-03-12 00:14:55 -0400

    cambio de directorio

--- Tarea2/intToStr.asm
+++ Tarea2/intToStr.asm
@@ -1,93 +1,69 @@
-section .data
-	text1 db "Ingrese un numero", 0xA ;len 18
-	digitos db '0123456789ABCDEF'
-
 section .bss
-	number resb 21
-	num1 resb 101
+	number resb 30 ; Reserva 30 bytes para el numero a convertir (cambiar si se quiere hacer mas grande)
 	
 section .text
 global _start
 
 _start:
-	call _printText1	;Imprime el texto inicial
-	call _getText		;Obtiene el texto del teclado
-	call _ATOI
+	; inicializa el valor inicial en rax
+	mov rax, 4327890  
+	; Carga la direccion de memoria de "number" en rsi
+	mov rsi, number
+	; Llama a la funcion para convertir a caracteres ASCII
+	call __to_string
 	
-
-	;call __to_string
-
-	call _finishCode
-
-_printText1:			;texto inicial
+	;Imprimir el resultado
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, text1
-	mov rdx, 18
-	syscall 
-	ret
-
-_getText:			;obtiene el texto
-	mov rax, 0
-	mov rdi, 0
-	mov rsi, num1
-	mov rdx, 101
-	syscall 
-	ret
-
-_ATOI:
-	mov rax, num1
-	xor rbx, rbx
-	xor rax, rax
-	lea rcx, [num1]
-	mov bl, byte[rcx]
-	ret
-
-__to_string:
-	push rax
+	mov rsi, number
+	mov rdx, 30  ; cambiar esto si se quiere un num mas grande
+	syscall
 	
-	mov rdi, 1
-	mov rcx, 1
-	mov rbx, 10
-	get_divisor:
-		xor rdx, rdx
-		div rbx  ; rax = 4
-		
-		cmp rax, 0 ; false
-		je _after
-		imul rcx, 10  ; 100
-		inc rdi
-		jmp get_divisor
-		
-		
-	_after:
-		pop rax
-		push rdi
-		
-	to_string:
-		xor rdx, rdx
-		div rcx ; RDX = 32, RAX = 4
-		
-		add al, "0" ;52 = "4"
-		mov [rsi], al
-		inc rsi
-		
-		push rdx
-		xor rdx, rdx
-		mov rax, rcx
-		mov rbx, 10
-		div rbx
-		mov rcx, rax
-		
-		pop rax
-		
-		cmp rcx, 0
-		jg to_string
-		
-	pop rdx
-	ret
-
-_finishCode:			;finaliza codigo
+	;Salir del programa
 	mov rax, 60
 	mov rdi, 0
 	syscall
+	
+__to_string:
+	push rax ; Guarda el valor de rax en la pila
+	
+	mov rdi, 1
+	mov rcx, 1 ; contador de digitos a 1
+	mov rbx, 10 ; base para la division
+	get_divisor:
+		xor rdx, rdx ; limpia rdx para preparar la division
+		div rbx  ; divide rax por 10, resultado en rax y residuo en rdx
+		
+		cmp rax, 0 ; comprueba si el cociente es cero
+		je _after ; termina bucle si es 0
+		imul rcx, 10  ; multiplica contador por 10
+		inc rdi ; incrementa el contador
+		jmp get_divisor ; vuelve al inicio del bucle
+		
+		
+	_after:
+		pop rax ; recupera el valor original de rax
+		push rdi ; guarda el contador de digitos en la pila
+		
+	to_string:
+		xor rdx, rdx ; limpia rdx para preparar la division
+		div rcx ; divide el valor original de rax por el contador de digitos
+		
+		add al, "0" ; Convierte digito a su representacion en ASCII
+		mov [rsi], al ; almacena digito a la posicion de memoria
+		inc rsi ; incrementa el puntero a la siguiente posicion de memoria
+		
+		push rdx ; guarda el residuo de la division en la pila
+		xor rdx, rdx ; limpia rdx
+		mov rax, rcx ; restaura rax
+		mov rbx, 10 ; establece la base para la division siguiente
+		div rbx ; divide el valor original de rax por 10
+		mov rcx, rax ; actualiza el contador con el nuevo valor de rax
+		
+		pop rax ; recupera el residuo de la pila
+		
+		cmp rcx, 0 ; Comprueba que se han procesado todos los digitos
+		jg to_string ; continua el bucle si aun no se procesan todos los digitos
+		
+	pop rdx ; limpia residuo de la pila
+	ret ; retorna de la funcion
